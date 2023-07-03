85ad21d765670429034fcc16aa59e0207707b166
Tomasz Cekało <tomasz.cekalo@gmail.com>
2023-07-03 10:55:07 +0200

    Version Update Support for Serialization

--- Undine.DefaultEcs/DefaultEcsContainer.cs
+++ Undine.DefaultEcs/DefaultEcsContainer.cs
@@ -20,8 +20,10 @@ namespace Undine.DefaultEcs
             _runner = new DefaultParallelRunner(Environment.ProcessorCount);
             _systems = new List<AEntitySetSystem<float>>();
         }
+
         public override void AddSystem<T>(UnifiedSystem<T> system)
         {
+            RegisterComponentType<T>();
             _systems.Add(new DefaultSystem<T>(_world, _runner)
             {
                 System = system
@@ -30,6 +32,8 @@ namespace Undine.DefaultEcs
 
         public override void AddSystem<A, B>(UnifiedSystem<A, B> system)
         {
+            RegisterComponentType<A>();
+            RegisterComponentType<B>();
             _systems.Add(new DefaultSystem<A, B>(_world, _runner)
             {
                 System = system
@@ -38,6 +42,9 @@ namespace Undine.DefaultEcs
 
         public override void AddSystem<A, B, C>(UnifiedSystem<A, B, C> system)
         {
+            RegisterComponentType<A>();
+            RegisterComponentType<B>();
+            RegisterComponentType<C>();
             _systems.Add(new DefaultSystem<A, B, C>(_world, _runner)
             {
                 System = system
@@ -46,6 +53,10 @@ namespace Undine.DefaultEcs
 
         public override void AddSystem<A, B, C, D>(UnifiedSystem<A, B, C, D> system)
         {
+            RegisterComponentType<A>();
+            RegisterComponentType<B>();
+            RegisterComponentType<C>();
+            RegisterComponentType<D>();
             _systems.Add(new DefaultSystem<A, B, C, D>(_world, _runner)
             {
                 System = system
@@ -59,6 +70,7 @@ namespace Undine.DefaultEcs
 
         public override ISystem GetSystem<A>(UnifiedSystem<A> system)
         {
+            RegisterComponentType<A>();
             return new DefaultSystem<A>(_world, _runner, 2)
             {
                 System = system
@@ -67,6 +79,8 @@ namespace Undine.DefaultEcs
 
         public override ISystem GetSystem<A, B>(UnifiedSystem<A, B> system)
         {
+            RegisterComponentType<A>();
+            RegisterComponentType<B>();
             return new DefaultSystem<A, B>(_world, _runner, 2)
             {
                 System = system
@@ -75,6 +89,9 @@ namespace Undine.DefaultEcs
 
         public override ISystem GetSystem<A, B, C>(UnifiedSystem<A, B, C> system)
         {
+            RegisterComponentType<A>();
+            RegisterComponentType<B>();
+            RegisterComponentType<C>();
             return new DefaultSystem<A, B, C>(_world, _runner, 2)
             {
                 System = system
@@ -83,6 +100,10 @@ namespace Undine.DefaultEcs
 
         public override ISystem GetSystem<A, B, C, D>(UnifiedSystem<A, B, C, D> system)
         {
+            RegisterComponentType<A>();
+            RegisterComponentType<B>();
+            RegisterComponentType<C>();
+            RegisterComponentType<D>();
             return new DefaultSystem<A, B, C, D>(_world, _runner, 2)
             {
                 System = system
